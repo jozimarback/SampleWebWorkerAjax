fd84198c8f0571f86929c6fda829d0086579b80c
Jozimar Back <jozimar5@gmail.com>
2016-09-25 15:14:23 -0300

    Create workerAjax.js
    Criação de arquivo worker :)

---
+++ workerAjax.js
@@ -0,0 +1,61 @@
+self.addEventListener('message', function (e) {
+
+    fetch(e.data, function (xhr) {
+        var result = xhr.responseText;
+        //process the JSON
+        var object = JSON.parse(result);
+        //set a timeout just to add some latency
+        setTimeout(function () { sendback(); }, 2000);
+        //pass JSON object back as string
+        function sendback() {
+            self.postMessage(JSON.stringify(object));
+        }
+    });
+
+}, false);
+
+
+//simple XHR request in pure raw JavaScript
+function fetch(objetoRequisicao, callback) {
+    var xhr;
+
+    if (typeof XMLHttpRequest !== 'undefined') xhr = new XMLHttpRequest();
+    else {
+        var versions = ["MSXML2.XmlHttp.5.0",
+                        "MSXML2.XmlHttp.4.0",
+                        "MSXML2.XmlHttp.3.0",
+                        "MSXML2.XmlHttp.2.0",
+                        "Microsoft.XmlHttp"]
+
+        for (var i = 0, len = versions.length; i < len; i++) {
+            try {
+                xhr = new ActiveXObject(versions[i]);
+                break;
+            }
+            catch (e) { }
+        }
+    }
+
+    xhr.onreadystatechange = ensureReadiness;
+
+    function ensureReadiness() {
+        if (xhr.readyState < 4) {
+            return;
+        }
+
+        if (xhr.status !== 200) {
+            return;
+        }
+
+        if (xhr.readyState === 4) {
+            callback(xhr);
+        }
+    }
+
+    xhr.open(objetoRequisicao.type, objetoRequisicao.url, true);
+    
+    if (objetoRequisicao.type === "POST") {
+        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    }
+    xhr.send(objetoRequisicao.data);
+}
